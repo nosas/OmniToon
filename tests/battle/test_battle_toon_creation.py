28e15ff5784aaa8f24abee56323516305242c341
Sason <sasonreza@gmail.com>
2021-07-20 00:41:58 -0700

    Correct false positives
    I forgot the assert keyword for two tests..

--- tests/battle/test_battle_toon_creation.py
+++ tests/battle/test_battle_toon_creation.py
@@ -14,10 +14,11 @@ class TestBattleToonCreation:
         assert BT.name == NAME == BT.entity.name
 
     def test_battle_toon_creation_default_hp(self):
-        BT.hp == DEFAULT_HP
+        assert BT.hp == DEFAULT_HP == BT.entity.hp
 
     def test_battle_toon_creation_default_inventory(self):
-        BT.hp == Inventory()
+        assert BT.entity.inventory == Inventory()
+        assert BT.entity.inventory.has_gags() is False
 
     def test_battle_toon_creation_default_battle_id(self):
         assert BT.battle_id == BATTLE_ID
