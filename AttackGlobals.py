f8b9a69cf4d976574a4f7d00086902d5f39296aa
Sason <sasonreza@gmail.com>
2021-07-27 21:46:22 -0700

    Create MULTIPLIER Enum

--- AttackGlobals.py
+++ AttackGlobals.py
@@ -6,3 +6,12 @@ class GROUP(Enum):
     UNKNOWN = 0
     SINGLE = 1
     MULTI = 2
+
+
+class MULTIPLIER(Enum):
+    FLOOR1 = 1
+    FLOOR2 = 2
+    FLOOR3 = 3
+    FLOOR4 = 4
+    FLOOR5 = 5
+    INVASION = 2
