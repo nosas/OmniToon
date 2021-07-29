08f494ba4f4d6d85abf1ac7b1ec7d112f2742050
Sason <sasonreza@gmail.com>
2021-07-29 00:11:43 -0700

    Write tests for BattleCogFactory
    Up next:
    - Write tests for LuredBattleCogFactory and TrappedBattleCogFactory

--- tests/battle/test_battle_cog_creation.py
+++ tests/battle/test_battle_cog_creation.py
@@ -1,6 +1,7 @@
 from ...Battle import BattleCog
 from ...Cog import Cog
 from ...Entity import BattleEntity, Entity
+from ...Factory import BattleEntityFactory, CogFactory
 
 BATTLE_ID = 1
 KEY = 'f'
@@ -57,6 +58,42 @@ class TestBattleCogCreation:
 
     cog = Cog(key=KEY, relative_level=relative_level)
     bc = BattleCog(battle_id=BATTLE_ID, entity=cog)
+
+    def test_battle_cog_default_creation_battle_id(self):
+        assert self.bc.battle_id == BATTLE_ID
+
+    def test_battle_cog_default_creation_hp(self):
+        assert self.bc.hp == self.hp
+
+    def test_battle_cog_default_creation_name(self):
+        assert self.bc.name == NAME
+
+    def test_battle_cog_default_creation_key(self):
+        assert self.bc.entity.key == self.bc.key == KEY
+
+    def test_battle_cog_default_creation_level(self):
+        assert self.bc.entity.level == self.bc.level == self.level
+
+    def test_battle_cog_default_creation_relative_level(self):
+        assert self.bc.entity.relative_level == self.bc.relative_level == self.relative_level
+
+    def test_battle_cog_default_creation_type(self):
+        assert isinstance(self.bc, BattleCog)
+        assert isinstance(self.bc, BattleEntity)
+        assert isinstance(self.bc.entity, Cog)
+        assert isinstance(self.bc.entity, Entity)
+
+
+class TestBattleCogFactoryCreation:
+    hp = 6
+    level = 1
+    relative_level = 0
+
+    c_factory = CogFactory()
+    bc_factory = BattleEntityFactory()
+
+    cog = c_factory.get_cog(key=KEY, relative_level=relative_level)
+    bc = bc_factory.get_battle_entity(battle_id=BATTLE_ID, entity=cog)
 
     def test_battle_cog_default_creation_battle_id(self):
         assert self.bc.battle_id == BATTLE_ID
