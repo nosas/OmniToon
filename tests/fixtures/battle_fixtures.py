11125f9baa4074920e7e5cbd280f74d90665e716
Sason <sasonreza@gmail.com>
2021-07-29 19:38:20 -0700

    Ad docstring and remove unused imports

--- tests/fixtures/battle_fixtures.py
+++ tests/fixtures/battle_fixtures.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ...Battle import BattleCog, BattleToon
+from ...Battle import BattleToon
 from ...Cog import Cog, get_random_cog
 from ...Factory import BattleCogFactory
 from ...Toon import Toon
@@ -63,6 +63,14 @@ def bc_trapped(request):
 
 
 def get_cog_from_request_param(request_param) -> Cog:
+    """Given a pytest request containing a Cog or a (key, rel_lvl) tuple, return a Cog object
+
+    Args:
+        request_param (Cog | Tuple[str, int]): Cog object or a (key, rel_lvl) tuple
+
+    Returns:
+        Cog: Cog object
+    """
     if isinstance(request_param, tuple):
         return Cog(key=request_param[0], relative_level=request_param[1])
     elif isinstance(request_param, Cog):
